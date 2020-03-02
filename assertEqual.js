e163846c2f30eed443ab7116f7897037bb44733a
Jeff Sexton <sexton.jeffp@gmail.com>
2020-03-02 23:23:08 +0000

    Created custom console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅👍 - Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🔴(╯°□°）╯︵ ┻━┻ - Assertion Failed: ' + actual + ' != ' + expected);
+  }
+
+};
+
+//Test Code
+
+
+assertEqual('identical string', 'identical string');
+assertEqual("lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
