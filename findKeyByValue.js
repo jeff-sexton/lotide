7a43b303a5c0fa67c3a68bf975a25fc76d1ea08c
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-04 14:01:23 -0800

    created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅👍 - Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🔴(╯°□°）╯︵ ┻━┻ - Assertion Failed: ${actual} != ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(object, searchValue) {
+  for (let [key, value] of Object.entries(object)) {
+    if (value === searchValue) {
+      return key;
+    }
+  }
+};
+
+
+
+
+
+//Test Cases
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
