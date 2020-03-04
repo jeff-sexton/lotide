23f7b1aca895d3751c99fd96baf74b8c96a5b9fe
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-04 13:07:54 -0800

    created countLetters function

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅👍 - Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🔴(╯°□°）╯︵ ┻━┻ - Assertion Failed: ${actual} != ${expected}`);
+  }
+
+};
+
+const countLetters = function(sentence) {
+  let cleanString = sentence.split(' ').join('').toLowerCase();
+  console.log(cleanString);
+  let countObject = {};
+
+  for (let letter of cleanString) {
+    if (countObject[letter]) {
+      countObject[letter] += 1;
+    } else {
+      countObject[letter] = 1;
+    }
+  }
+
+  return countObject;
+
+};
+
+
+assertEqual(countLetters('LHL')['l'], 2);
+assertEqual(countLetters('LHL')['h'], 1);
+
+console.log(countLetters('LHL'));
+
+console.log(countLetters("lighthouse in the house"));
+
