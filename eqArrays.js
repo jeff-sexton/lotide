2bc49baed6a6b86dc1bef9af693f024c8998a45f
Jeff Sexton <sexton.jeffp@gmail.com>
2020-03-06 22:10:10 +0000

    Refactored to test nested arrays using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -12,11 +12,18 @@ const eqArrays = function(array1, array2) {
     return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+      if (Array.isArray(array1[i])) {
+        if (Array.isArray(array2[i])) {
+          return eqArrays(array1[i], array2[i]);
+        } else {
+          return false;
+        }
+
+      } else if (array1[i] !== array2[i]) {
         return false;
+
       }
     }
-    
   }
 
   return true;
@@ -44,3 +51,9 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, [2, 3], 3], [1, [2, 3], 3]), true);
+
+assertEqual(eqArrays([1, [2, 3, 4], 3], [1, [2, 3, 4, 5], 3]), false);
+
+assertEqual(eqArrays([1, [2, 3], 3], [1, 2, 3]), false);
