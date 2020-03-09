f45f5982213c63cfde07a312ef816e2938662c44
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-09 12:33:36 -0700

    Refactored Middle tests to seperate file

--- middle.js
+++ middle.js
@@ -13,18 +13,3 @@ const middle = function(array) {
 };
 
 module.exports = middle;
-
-// Test Code:
-
-// const assertArraysEqual = require('./assertArraysEqual');
-
-// assertArraysEqual(middle([1]), []); // => []
-// assertArraysEqual(middle([1, 2]), []); // => []
-
-
-// assertArraysEqual(middle([1, 2, 3]), [2]);// => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
